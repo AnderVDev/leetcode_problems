37dbcb214c61231fd55ac09438f7186122f6a651
Anderson <avictoral@gmail.com>
2023-12-20 12:43:55 -0500

    updated problem 80

--- 80.RemoveDuplicatesfromSortedArrayI/Solution.java
+++ 80.RemoveDuplicatesfromSortedArrayI/Solution.java
@@ -1,10 +1,20 @@
 public class Solution {
 
+    public int removeDuplicates(int[] nums) {
+        int writer = 1;
+        for (int reader = 1; reader < nums.length; reader++) {
+            if (nums[reader - 1] != nums[reader] || (reader > 2 && nums[reader - 2] != nums[reader - 1])) {
+                nums[writer++] = nums[reader];
+            }
+        }
 
-
+        return writer;
+    }
 
     public static void main(String[] args) {
-        // Solution solution = new Solution();
+        Solution solution = new Solution();
+        int [] nums = {1,1,1,2,2,3};
+        solution.removeDuplicates(nums);
     }
-    
+
 }

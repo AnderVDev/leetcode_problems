945d87e258459f8e71af8af34fd611ebd3828cf9
Anderson <avictoral@gmail.com>
2023-12-21 14:37:36 -0500

    resolved problem 80

--- 80.RemoveDuplicatesfromSortedArrayI/Solution.java
+++ 80.RemoveDuplicatesfromSortedArrayI/Solution.java
@@ -5,12 +5,13 @@ public class Solution {
         int writer = 1, counter = 1;
         for (int reader = 1; reader < nums.length; reader++) {
 
-            counter = nums[reader - 1] == nums[reader] ? counter++ : 1;
+            counter = nums[reader - 1] == nums[reader] ? counter + 1 : 1;
+            System.out.println("Counter: " + counter );
 
             if (counter <= 2) {
                 nums[writer++] = nums[reader];
             }
-            System.out.println("Array Status: " + Arrays.toString(nums));
+            // System.out.println("Array Status: " + Arrays.toString(nums));
         }
         System.out.println("Result: " + Arrays.toString(nums));
         return writer;

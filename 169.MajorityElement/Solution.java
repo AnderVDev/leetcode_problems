a838d32332aceddfd12201d6a22694b1b3acfe78
Anderson <avictoral@gmail.com>
2023-12-21 18:37:19 -0500

    solved problem 169 with brute force approach

--- 169.MajorityElement/Solution.java
+++ 169.MajorityElement/Solution.java
@@ -1,10 +1,26 @@
 public class Solution {
 
+    public int majorityElement(int[] nums) {
+        int majority = nums.length / 2;
+        for (int num : nums) {
+            int counter = 0;
+            for (int currentValue : nums) {
+                if (num == currentValue)
+                    counter++;
 
-
+            }
+            if (counter > majority)
+                return num;
+        }
+        return -1;
+    }
 
     public static void main(String[] args) {
-        // Solution solution = new Solution();
+        Solution solution = new Solution();
+        // int [] nums = {3,2,3};
+        int[] nums = { 2, 2, 1, 1, 1, 2, 2 };
+        int sol = solution.majorityElement(nums);
+        System.out.println(sol);
     }
-    
+
 }

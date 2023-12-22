2a8e61d4f546a40a2ec7245c2d53536ec373715d
Anderson <avictoral@gmail.com>
2023-12-22 16:34:05 -0500

    solved problem 14

--- 14.LongestCommonPrefix/Solution.java
+++ 14.LongestCommonPrefix/Solution.java
@@ -1,10 +1,23 @@
 public class Solution {
 
+    // Approach 1: Horizontal scanning
+    public String longestCommonPrefix(String[] strs) {
+        String prefix = strs[0];
 
+        for (int index = 1; index < strs.length; index++) {
 
+            while (strs[index].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
+        }
+
+        return prefix;
+    }
 
     public static void main(String[] args) {
-        // Solution solution = new Solution();
+        Solution solution = new Solution();
+        String[] strs = { "flower", "flow", "flight" };
+        System.out.println(solution.longestCommonPrefix(strs));
     }
-    
+
 }

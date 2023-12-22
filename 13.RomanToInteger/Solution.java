bdcefc97f1618cfccb5f690d73b0002d11294b4b
Anderson <avictoral@gmail.com>
2023-12-22 08:02:45 -0500

    solved problem 13 right to left approach

--- 13.RomanToInteger/Solution.java
+++ 13.RomanToInteger/Solution.java
@@ -2,7 +2,37 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Solution {
+    // Approach 1: Left-to-Right Pass
+    // public int romanToInt(String s) {
+    // Map<String, Integer> values = new HashMap<>();
+    // values.put("M", 1000);
+    // values.put("D", 500);
+    // values.put("C", 100);
+    // values.put("L", 50);
+    // values.put("X", 10);
+    // values.put("V", 5);
+    // values.put("I", 1);
 
+    // int add = 0;
+    // int reader = 0;
+    // String input = s.toUpperCase();
+    // String currentString;
+    // while (reader < s.length()) {
+
+    // currentString = input.substring(reader, reader + 1);
+    // int currentValue = values.get(currentString);
+    // int nextValue = 0;
+    // if (reader + 1 < input.length()) {
+    // String nextString = input.substring(reader + 1, reader + 2);
+    // nextValue = values.get(nextString);
+    // }
+    // add += nextValue > currentValue ? nextValue - currentValue : currentValue;
+    // reader += nextValue > currentValue ? 2 : 1;
+    // }
+    // return add;
+    // }
+
+    // Approach 2: Right-to-left Pass
     public int romanToInt(String s) {
         Map<String, Integer> values = new HashMap<>();
         values.put("M", 1000);
@@ -13,23 +43,17 @@ public class Solution {
         values.put("V", 5);
         values.put("I", 1);
 
-        int add = 0;
-        int reader = 0;
-        String input = s.toUpperCase();
-        String currentString;
-        while (reader < s.length()) {
+        int lastValue = values.get(s.substring(s.length() - 1));
+        int total = lastValue;
 
-            currentString = input.substring(reader, reader + 1);
+        for (int reader = s.length() - 2; reader >= 0; reader--) {
+            String currentString = s.substring(reader, reader + 1);
             int currentValue = values.get(currentString);
-            int nextValue = 0;
-            if (reader + 1 < input.length()) {
-                String nextString = input.substring(reader + 1, reader + 2);
-                nextValue = values.get(nextString);
-            }
-            add += nextValue > currentValue ? nextValue - currentValue : currentValue;
-            reader += nextValue > currentValue ? 2 : 1;
+            total = currentValue < lastValue ? total - currentValue : total + currentValue;
+            lastValue = currentValue;
         }
-        return add;
+        return total;
+
     }
 
     public static void main(String[] args) {

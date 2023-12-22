d50f7ceaa6ac74f6e4ab6c5084f54ee51b93847f
Anderson <avictoral@gmail.com>
2023-12-21 19:31:00 -0500

    update solution 169 with HashMap

--- 169.MajorityElement/Solution.java
+++ 169.MajorityElement/Solution.java
@@ -1,16 +1,37 @@
+import java.util.HashMap;
+
 public class Solution {
+    // Approach 1: Brute Force
+    // public int majorityElement(int[] nums) {
+    // int majority = nums.length / 2;
+    // for (int num : nums) {
+    // int counter = 0;
+    // for (int currentValue : nums) {
+    // if (num == currentValue)
+    // counter++;
+
+    // }
+    // if (counter > majority)
+    // return num;
+    // }
+    // return -1;
+    // }
+
+    // Approach 2: HashMap
+    private HashMap<Integer, Integer> countNums(int[] nums) {
+        HashMap<Integer, Integer> counts = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            counts.put(num, !counts.containsKey(num) ? 1 : counts.get(num) + 1);
+        }
+        return counts;
+    }
 
     public int majorityElement(int[] nums) {
-        int majority = nums.length / 2;
-        for (int num : nums) {
-            int counter = 0;
-            for (int currentValue : nums) {
-                if (num == currentValue)
-                    counter++;
+        HashMap<Integer, Integer> counts = countNums(nums);
 
-            }
-            if (counter > majority)
-                return num;
+        for (int key : counts.keySet()) {
+            if (counts.get(key) > nums.length / 2)
+                return key;
         }
         return -1;
     }

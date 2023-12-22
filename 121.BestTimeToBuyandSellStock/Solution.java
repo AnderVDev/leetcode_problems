6f306d9a574ebd535a5b54b3e31064804d53a2e7
Anderson <avictoral@gmail.com>
2023-12-21 20:34:07 -0500

    resolved problem 121 with One pass approach

--- 121.BestTimeToBuyandSellStock/Solution.java
+++ 121.BestTimeToBuyandSellStock/Solution.java
@@ -1,12 +1,28 @@
 public class Solution {
+    // Approach 1: Brute Force
+    // public int maxProfit(int[] prices) {
+    // int profit = 0;
+    // for (int buy = 0; buy < prices.length; buy++) {
+    // for (int sell = 1 + buy ; sell < prices.length; sell++) {
+    // int pnl = prices[sell] - prices[buy];
+    // if (pnl > profit)
+    // profit = pnl;
+    // }
+    // }
+    // return profit;
+    // }
+
+    // Approach 2: One Pass
     public int maxProfit(int[] prices) {
         int profit = 0;
-        for (int buy = 0; buy < prices.length; buy++) {
-            for (int sell = buy + 1; sell < prices.length; sell++) {
-                int pnl = prices[sell] - prices[buy];
-                if (pnl > profit)
-                    profit = pnl;
-            }
+        int minPrice = Integer.MAX_VALUE;
+
+        for (int price : prices) {
+            if (price < minPrice)
+                minPrice = price;
+            else if (price - minPrice > profit)
+                profit = price - minPrice;
+
         }
         return profit;
     }

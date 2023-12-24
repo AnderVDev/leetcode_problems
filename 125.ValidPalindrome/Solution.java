f44ab43d210ca95df3e34dcf465e24b40d55e910
Anderson <avictoral@gmail.com>
2023-12-24 10:35:16 -0500

    created problem 125.ValidPalindrome

---
+++ 125.ValidPalindrome/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public boolean isPalindrome(String s) {
+
+        return false;
+        
+    }
+
+
+
+
+    public static void main(String[] args) {
+        // Solution solution = new Solution();
+    }
+    
+}

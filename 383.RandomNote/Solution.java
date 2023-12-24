496c13f214b58bd7179a429addba29b9033fdbfb
Anderson <avictoral@gmail.com>
2023-12-24 17:36:16 -0500

    updated solution 383

--- 383.RandomNote/Solution.java
+++ 383.RandomNote/Solution.java
@@ -3,16 +3,60 @@ import java.util.Map;
 
 public class Solution {
 
-    public boolean canConstruct(String ransomNote, String magazine) {
-        Map<Character, Integer> magazineMap = new HashMap<>();
+    // Simulation Approach
 
-        for (char i : magazine.toCharArray()) {
-            magazineMap.put(i, magazineMap.containsKey(i) ? magazineMap.get(i) + 1 : 1);
+    // public boolean canConstruct(String ransomNote, String magazine) {
+    //     for (char c : ransomNote.toCharArray()) {
+
+    //     int index = magazine.indexOf(c);
+
+    //     if (index == -1) {
+    //         return false;
+    //     }
+ 
+    //     magazine = magazine.substring(0, index) + magazine.substring(index + 1);
+    // }
+   
+    // return true;
+    // }
+
+    
+
+    // public boolean canConstruct(String ransomNote, String magazine) {
+    //     Map<Character, Integer> magazineMap = new HashMap<>();
+
+    //     for (char i : magazine.toCharArray()) {
+    //         magazineMap.put(i, magazineMap.containsKey(i) ? magazineMap.get(i) + 1 : 1);
+    //     }
+
+    //     // for (char i : magazineMap.keySet()){
+    //     // System.out.println("Key: "+ i + " Counter:" + magazineMap.get(i));
+    //     // }
+
+    //     for (char c : ransomNote.toCharArray()) {
+    //         if (!magazineMap.containsKey(c) || magazineMap.get(c) == 0) {
+    //             return false;
+    //         }
+    //         magazineMap.put(c, magazineMap.get(c) - 1);
+    //     }
+
+    //     return true;
+    // }
+
+
+    public Map<Character, Integer> counterMap(String string){
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : string.toCharArray()) {
+            map.put(c, map.containsKey(c) ? map.get(c) + 1 : 1);
         }
+        return map;
+    }
 
-        // for (char i : magazineMap.keySet()){
-        // System.out.println("Key: "+ i + " Counter:" + magazineMap.get(i));
-        // }
+
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        Map<Character, Integer> magazineMap = counterMap(magazine);
 
         for (char c : ransomNote.toCharArray()) {
             if (!magazineMap.containsKey(c) || magazineMap.get(c) == 0) {
@@ -26,10 +70,10 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String ransomNote = "aa", magazine = "aab";
+        // String ransomNote = "aa", magazine = "aab";
         // String ransomNote = "a", magazine = "b";
         // String ransomNote = "aa", magazine = "ab";
-        // String ransomNote = "aab", magazine = "baa";
+        String ransomNote = "aab", magazine = "baa";
         System.out.println(solution.canConstruct(ransomNote, magazine));
     }
 

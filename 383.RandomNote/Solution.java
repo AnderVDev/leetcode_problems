acda150c657416aa91fe7c60b3ad9aca9dda6747
Anderson <avictoral@gmail.com>
2023-12-24 17:23:58 -0500

    solved problem 383 one hashMap

--- 383.RandomNote/Solution.java
+++ 383.RandomNote/Solution.java
@@ -1,18 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
 
     public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character, Integer> magazineMap = new HashMap<>();
 
+        for (char i : magazine.toCharArray()) {
+            magazineMap.put(i, magazineMap.containsKey(i) ? magazineMap.get(i) + 1 : 1);
+        }
 
-        return false;
+        // for (char i : magazineMap.keySet()){
+        // System.out.println("Key: "+ i + " Counter:" + magazineMap.get(i));
+        // }
+
+        for (char c : ransomNote.toCharArray()) {
+            if (!magazineMap.containsKey(c) || magazineMap.get(c) == 0) {
+                return false;
+            }
+            magazineMap.put(c, magazineMap.get(c) - 1);
+        }
+
+        return true;
     }
-
-
-
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String ransomNote = "a", magazine = "b";
+        String ransomNote = "aa", magazine = "aab";
+        // String ransomNote = "a", magazine = "b";
+        // String ransomNote = "aa", magazine = "ab";
+        // String ransomNote = "aab", magazine = "baa";
         System.out.println(solution.canConstruct(ransomNote, magazine));
     }
-    
+
 }

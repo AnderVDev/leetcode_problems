1ee1cc79609a86aa67f0e100d13d993c6a6a3487
Anderson <avictoral@gmail.com>
2023-12-24 16:16:04 -0500

    created problem 383.RandomNote

---
+++ 383.RandomNote/Solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+
+        return false;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String ransomNote = "a", magazine = "b";
+        System.out.println(solution.canConstruct(ransomNote, magazine));
+    }
+    
+}

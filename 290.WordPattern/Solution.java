6417e0f2ab87aae1749c042d547c23b1252021f8
Anderson <avictoral@gmail.com>
2023-12-27 11:04:16 -0500

    solved problem 290

--- 290.WordPattern/Solution.java
+++ 290.WordPattern/Solution.java
@@ -1,10 +1,57 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
 
+    public String transformation(String string) {
+        StringBuilder builder = new StringBuilder();
+        String[] words = string.split("\\s+");
+        System.out.println(words.length);
+        if (words.length > 1 ) {
+            Map<String, Integer> mapper = new HashMap<>();
+            for (int index = 0; index < words.length; index++) {
+                String word = words[index];
+                if (!mapper.containsKey(word)) {
+                    mapper.put(word, index);
+                }
+                builder.append(mapper.get(word));
+                builder.append("-");
+                System.out.println(builder.toString());
+            }
+        } else {
+            Map<Character, Integer> mapper = new HashMap<>();
+            for (int i = 0; i < string.length(); i++) {
+                char c = string.charAt(i);
+                if (!mapper.containsKey(c)) {
+                    mapper.put(c, i);
+                }
 
+                builder.append(mapper.get(c));
+                builder.append("-");
+                System.out.println(builder.toString());
+            }
+        }
+        return builder.toString();
+    }
 
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split("\\s+"); 
+        if(s.length() < pattern.length() || pattern.equals(s) && words.length != pattern.length()) return false;
+        else if(pattern.toCharArray().length == 1 && words.length == 1 ) return true;
+        String patternT = transformation(pattern);
+        String sT = transformation(s);
+
+        return patternT.equals(sT);
+    }
 
     public static void main(String[] args) {
-        // Solution solution = new Solution();
+        Solution solution = new Solution();
+        // String pattern = "abba", s = "dog cat cat dog";
+        // String pattern = "abba", s = "dog cat cat fish";
+        // String pattern = "e", s = "eukera";
+        String pattern = "jquery", s = "jquery";
+        System.out.println(solution.wordPattern(pattern, s));
+
     }
-    
+
 }

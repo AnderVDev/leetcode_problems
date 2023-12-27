7c3968279d01b7d79adc9de4130963970ac0b9a0
Anderson <avictoral@gmail.com>
2023-12-27 12:35:53 -0500

    created and solved problem 242.ValidAnagram

---
+++ 242.ValidAnagram/Solution.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class Solution {
+
+    // public boolean isAnagram(String s, String t) {
+    //     if(s.length() != t.length()) return false;
+    //     Map<Character, Integer> sMap = counterMap(s);
+    //     Map<Character, Integer> tMap = counterMap(t);
+    //     for(char c : sMap.keySet()){
+    //         if(!tMap.containsKey(c) || sMap.get(c) != tMap.get(c)){
+    //             return false;
+    //         }
+    //     }
+    //     return true;        
+    // }
+
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+                
+        return Arrays.equals(sArray, tArray);        
+    }
+
+
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        // String s = "anagram", t = "nagaram";
+        String s = "rat", t = "car";
+        System.out.println(solution.isAnagram(s, t));
+
+    }
+    
+}

4ce9c5399005cf7e09695ba3f85384bdefbf5852
Anderson <avictoral@gmail.com>
2023-12-26 20:08:13 -0500

    updated problem 205

--- 205.IsomorphicStrings/Solution.java
+++ 205.IsomorphicStrings/Solution.java
@@ -1,12 +1,40 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
 
+    public Map<Character, Integer> mapper(String string){
+         Map<Character, Integer> mapper = new HashMap<>();
+         for(char c : string.toCharArray()){
+            mapper.put(c, mapper.containsKey(c) ? mapper.get(c)+1  : 1);
+         }
+         return mapper;
+
+    }
+
     public boolean isIsomorphic(String s, String t) {
-        return false;
+        Map<Character, Integer> sMap = mapper(s);
+        Map<Character, Integer> tMap = mapper(t);
+        int sWeight=0, tWeight=0;
+
+        for(char c : s.toCharArray()){
+            sWeight += sMap.get(c);
+            System.out.println("Key: " + c + " Counter: " + sMap.get(c));
+        }
+        for(char c : t.toCharArray()){
+            tWeight += tMap.get(c);
+            System.out.println("Key: " + c + " Counter: " + tMap.get(c));
+        }
+        System.out.println("SW: " + sWeight + " TW: " + tWeight);
+       
+        return tWeight == sWeight;
 
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
+        // String s = "egg", t = "add";
+        // String s = "foo", t = "bar";
         String s = "paper", t = "title";
         System.out.println(solution.isIsomorphic(s, t));
     }

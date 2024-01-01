97338dde18cc5d0f11dee3546b63acc01e2d07bf
Anderson <avictoral@gmail.com>
2024-01-01 12:18:19 -0500

    created problem 202HappyNumber

---
+++ 202.HappyNumber/Solution.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+
+    
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int n = 19;
+        System.out.println(solution.isHappy(n));
+    }
+
+}

76d4fb73a7473fb4fe8ed151a2d6b622533c4251
Anderson <avictoral@gmail.com>
2024-01-01 12:18:55 -0500

    solved problem 202

--- 202.HappyNumber/Solution.java
+++ 202.HappyNumber/Solution.java
@@ -3,7 +3,30 @@ import java.util.Set;
 
 public class Solution {
 
-    
+    public int nextValue(int n) {
+        int total = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            total += d * d;
+        }
+        System.out.println(total);
+        return total;
+    }
+
+    public boolean isHappy(int n) {
+        Set<Integer> values = new HashSet<>();
+        int num = n;
+
+        while (num != 1) {
+            if (values.contains(num)) {
+                return false;
+            }
+            values.add(num);
+            num = nextValue(num);
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
         Solution solution = new Solution();

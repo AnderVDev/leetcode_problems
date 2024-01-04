98e42bd21fc50ed3208eead7fce96ed486e38e05
Anderson <avictoral@gmail.com>
2024-01-04 17:33:27 -0500

    updated problem 228

--- 228.SummaryRanges/Solution.java
+++ 228.SummaryRanges/Solution.java
@@ -5,17 +5,25 @@ public class Solution {
 
     public List<String> summaryRanges(int[] nums) {
         List<String> ranges = new ArrayList<>();
-        int start = nums[0];
-        for(int i = 0; i < nums.length; i++ ){
-            
-        }
 
-        return ranges;        
+        for (int i = 0; i < nums.length; i++) {
+            int start = nums[i];
+
+            while (i + 1 < nums.length && nums[i] + 1 == nums[i + 1]) {
+                i++;
+            }
+            if (start != nums[i]) {
+                ranges.add(start + "->" + nums[i]);
+            } else {
+                ranges.add(String.valueOf(start));
+            }
+        }
+        return ranges;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] nums = {0,1,2,4,5,7};
+        int[] nums = { 0, 1, 2, 4, 5, 7 };
         System.out.println(solution.summaryRanges(nums));
     }
 

bc953294bd83b83979b3ef5b8ed1efb4bbd4f152
Anderson <avictoral@gmail.com>
2024-01-04 17:57:53 -0500

    updated solution 20

--- 20.ValidParentheses/Solution.java
+++ 20.ValidParentheses/Solution.java
@@ -4,42 +4,28 @@ public class Solution {
 
     private HashMap<Character, Character> mappings;
 
-  // Initialize hash map with mappings. This simply makes the code easier to read.
-  public Solution() {
-    this.mappings = new HashMap<Character, Character>();
-    this.mappings.put(')', '(');
-    this.mappings.put('}', '{');
-    this.mappings.put(']', '[');
-  }
-
-  public boolean isValid(String s) {
-
-    // Initialize a stack to be used in the algorithm.
-    Stack<Character> stack = new Stack<Character>();
-
-    for (int i = 0; i < s.length(); i++) {
-      char c = s.charAt(i);
-
-      // If the current character is a closing bracket.
-      if (this.mappings.containsKey(c)) {
-
-        // Get the top element of the stack. If the stack is empty, set a dummy value of '#'
-        char topElement = stack.empty() ? '#' : stack.pop();
-
-        // If the mapping for this bracket doesn't match the stack's top element, return false.
-        if (topElement != this.mappings.get(c)) {
-          return false;
-        }
-      } else {
-        // If it was an opening bracket, push to the stack.
-        stack.push(c);
-      }
+    public Solution() {
+        this.mappings = new HashMap<Character, Character>();
+        this.mappings.put(')', '(');
+        this.mappings.put('}', '{');
+        this.mappings.put(']', '[');
     }
 
-    // If the stack still contains elements, then it is an invalid expression.
-    return stack.isEmpty();
-  }
-
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (this.mappings.containsKey(c)) {
+                char topElement = stack.empty() ? '#' : stack.pop();
+                if (topElement != this.mappings.get(c)) {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
@@ -48,5 +34,4 @@ public class Solution {
         String s = "(]";
         System.out.println(solution.isValid(s));
     }
-    
 }
